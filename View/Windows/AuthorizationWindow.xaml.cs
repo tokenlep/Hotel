21bc4f82498a5c13b3aeaefed0adbf2820c3c62a
Арсений Вершинин <senya.1234567@mail.ru>
2024-11-05 16:12:38 +0300

    :)

--- View/Windows/AuthorizationWindow.xaml.cs
+++ View/Windows/AuthorizationWindow.xaml.cs
@@ -102,6 +102,7 @@ namespace Hotel.View.Windows
             if (Validation() == true)
             {
                 Authentication();
+                Authorization();
             }
         }
     }
