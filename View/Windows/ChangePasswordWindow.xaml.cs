2a4cf36efd04673a86473b8183c2ffa2df9c72d7
Арсений Вершинин <senya.1234567@mail.ru>
2024-11-08 14:06:16 +0300

    ...

--- View/Windows/ChangePasswordWindow.xaml.cs
+++ View/Windows/ChangePasswordWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Hotel.AppData;
+using System.Windows;
 
 namespace Hotel.View.Windows
 {
@@ -15,6 +16,16 @@ namespace Hotel.View.Windows
         private void ChangePasswordBtn_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        void  ChangePassword()
+        {
+            if (string.IsNullOrEmpty(OldPasswordPb.Password) ||
+                string.IsNullOrEmpty(NewPasswordPb.Password) ||
+                string.IsNullOrEmpty(AcceptNewPasswordPb.Password))
+            {
+                Feedback.Warning("Все поля обязательны для заполнения ! Заполните каждое поле !");
+            }
         }
     }
 }
